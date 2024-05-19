3e21987f184dfc320d51188be16661b34f47cd8e
BogdanTurbal <bogdan.turbal.y@gmail.com>
2024-05-19 20:19:03 +0300

    Lab 1 mod

---
+++ crypto_labs/test_lab_1.h
@@ -0,0 +1,12 @@
+#ifndef IO_UTILS_H
+#define IO_UTILS_H
+
+void run_prepared_scenario();
+void modular_exponentiation_test();
+void miller_rabin_test_choice();
+void baillie_psw_test_choice();
+void format_conversions_test();
+void find_primes_test();
+
+#endif // IO_UTILS_H
+
