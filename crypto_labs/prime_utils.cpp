cf667a823d1b63d8f0c9aef2f8f112b83e499704
BogdanTurbal <bogdan.turbal.y@gmail.com>
2024-05-19 19:11:24 +0300

    Initial Commit

---
+++ crypto_labs/prime_utils.cpp
@@ -0,0 +1,8 @@
+//
+//  prime_utils.cpp
+//  crypto_labs
+//
+//  Created by Bohdan Turbal on 19.05.2024.
+//
+
+#include <stdio.h>
---
+++ crypto_labs/prime_utils.h
@@ -0,0 +1,12 @@
+//
+//  prime_utils.h
+//  crypto_labs
+//
+//  Created by Bohdan Turbal on 19.05.2024.
+//
+
+#ifndef prime_utils_h
+#define prime_utils_h
+
+
+#endif /* prime_utils_h */
